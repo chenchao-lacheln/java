6c6bc04070267ae4a6356410199344cb7da6d3df
chenchao <chenchao@qq.com>
2022-04-04 01:17:52 +0800

    update homework02

--- javabase/src/com/chapter/seventh/Homework02.java
+++ javabase/src/com/chapter/seventh/Homework02.java
@@ -4,22 +4,25 @@ import java.util.Scanner;
 
 public class Homework02 {
     public static void main(String[] args) {
-        String[] array = {"aaa","bbbb","ccc","rrr","kkkk"};
+//        String[] array = {"aaa","bbbb","ccc","rrr","kkkk"};
+        String[] array = {};
         A02 p2 = new A02();
-        p2.find(array);
+        int index = p2.find("kkkk",array);
+        if (index == -1){
+            System.out.println("您查询的数据不存在！");
+        }else {
+            System.out.println("您查询的数据存在，index = " + index);
+        }
     }
 }
 //编写类A02，定义方法find，实现查找某字符串数组中的元素，并返回索引，如果找不到，返回01；
 class A02 {
-    public void find(String[] array){
-//        Scanner myScanner = new Scanner(System.in);
-//        System.out.println("请输入需要查找的字符串");
-//        String p1 = myScanner.next();
-        String p1 = "000";
-        for (int i = 0;i < array.length;i++){
-            if (p1.equals(array[i])){
-                System.out.println(i);
+    public int find(String findStr,String[] strs){
+        for (int i = 0;i < strs.length;i++){
+            if (findStr.equals(strs[i])){
+                return i;
             }
         }
+        return -1;
     }
 }
