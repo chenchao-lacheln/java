f864db7038e9e12fb256f7ad7d2c79d874050f28
chenchao <chenchao@qq.com>
2022-04-06 03:27:40 +0800

    add eighth good night !

---
+++ javabase/src/com/chapter/eighth/ArrayTest.java
@@ -0,0 +1,9 @@
+package com.chapter.eighth;
+
+import java.util.Scanner;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
