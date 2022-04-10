a99366905ed8f14717bfbea7d164bea9ff88c87b
chenchao <chenchao@qq.com>
2022-04-11 01:14:15 +0800

    当创建子类对象时，不管使用子类哪个构造器，默认情况下总会去调用父类的无参构造器

--- javabase/src/com/extend_/Base.java
+++ javabase/src/com/extend_/Base.java
@@ -9,9 +9,13 @@ public class Base {//父类
 
     //无参构造器
     public Base() {
-        System.out.println("子类base()的构造器被调用-----");
+        System.out.println("父类base()的构造器被调用-----");
     }
 
+    //编写一个有参构造器
+    public Base(String name,int ade){
+        System.out.println("父类Base(String name,int ade)的构造器被调用-----");
+    }
     //父类提供一个public的方法，返回n4
     public int getN4(){
         return n4;
--- javabase/src/com/extend_/Sub.java
+++ javabase/src/com/extend_/Sub.java
@@ -5,7 +5,13 @@ public class Sub extends Base{//子类
     public Sub() {//构造器
 //        super();// 默认调用父类的无参构造器（打比方：人出生了就会默认有个身份证））
 //        这里会默认有个super(); 调用父类的午餐构造器
-        System.out.println("父类Sub()构造器被调用-----");
+        System.out.println("子类Sub()构造器被调用-----");
+    }
+
+    //当创建子类对象时，不管使用子类哪个构造器，默认情况下总会去调用父类的无参构造器
+    public Sub(String name){
+        //do nothing....
+        System.out.println("字类Sub(String name)构造器被调用-----");
     }
 
     public void sayOk(){//子类方法
