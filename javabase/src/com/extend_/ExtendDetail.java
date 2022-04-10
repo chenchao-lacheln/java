2663c2abbd5d615ea4f378e2c879f1ead4cdcff4
chenchao <chenchao@qq.com>
2022-04-11 01:23:37 +0800

    父类没有指定默认构造器，子类必须在构造器中用super(lacheln,23);完成对父类的初始化工作

--- javabase/src/com/extend_/ExtendDetail.java
+++ javabase/src/com/extend_/ExtendDetail.java
@@ -4,23 +4,18 @@ public class ExtendDetail {
     public static void main(String[] args) {
 
         //子类必须调用父类的构造器，完成父类初始化
+        System.out.println("=====第一个对象=====");
         Sub sub = new Sub();//创建子类对象 sub
-        /**
-         * 父类和子类的构造器都被调用了（子类sub中默认有个super();会默认调用父类的构造器）
-         * 父类base()的构造器被调用-----
-         * 子类Sub()构造器被调用-----
-         */
 //        sub.sayOk();
 
-        //当创建子类对象时，不管使用子类哪个构造器，默认情况下总会去调用父类的无参构造器
         System.out.println("=====第二个对象=====");
         Sub sub1 = new Sub("lacheln");//创建子类对象 sub1
-
         /**
-         * 父类base()的构造器被调用-----
+         * =====第一个对象=====
+         * 父类Base(String name,int ade)的构造器被调用-----
          * 子类Sub()构造器被调用-----
          * =====第二个对象=====
-         * 父类base()的构造器被调用-----
+         * 父类Base(String name,int ade)的构造器被调用-----
          * 字类Sub(String name)构造器被调用-----
          */
 
