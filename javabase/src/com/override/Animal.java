2ece2aba8365ef14c517631bd0055db9970637c5
chenchao <chenchao@qq.com>
2022-04-18 23:37:28 +0800

    更新重弄写的细节

--- javabase/src/com/override/Animal.java
+++ javabase/src/com/override/Animal.java
@@ -4,4 +4,15 @@ public class Animal {
     public void cry(){
         System.out.println("动物叫唤");
     }
+
+    //父类中编写一个返回类型为Object的m1方法
+    public Object m1(){
+        return null;
+    }
+
+    //细节：子类方法不能缩小父类方法的访问权限
+    //public-protected-默认-privated
+    protected void eat(){
+
+    }
 }
--- javabase/src/com/override/Dog.java
+++ javabase/src/com/override/Dog.java
@@ -9,4 +9,22 @@ public class Dog extends Animal{
     public void cry(){
         System.out.println("小狗汪汪叫。。");
     }
+
+    //子类中编写一个返回类型为String的m1方法
+    /**
+     * 此时父类中的返回类型为Object，子类中的返回类型为String，因为是继承关系（父子关系），所以也构成了重写关系
+     * @return
+     */
+    public String m1(){
+        return null;
+    }
+
+//    //父类是public，此时这里子类是protected的话，就会报错，因为子类缩小了父类的访问范围
+//    protected void eat(){
+//    }
+    //细节：子类方法不能缩小父类方法的访问权限
+    //public-protected-默认-privated
+    public void eat(){
+
+    }
 }
