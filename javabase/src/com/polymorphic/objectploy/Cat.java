22eca4837f48904f5e45f7495ee75f2a9d19ca31
chenchao <chenchao@qq.com>
2022-04-20 00:57:01 +0800

    对象多态-的基本使用

---
+++ javabase/src/com/polymorphic/objectploy/Cat.java
@@ -0,0 +1,9 @@
+package com.polymorphic.objectploy;
+
+public class Cat extends Animal{
+
+    @Override//表示当前方法重写了父类的cry，@Override会做语法的校验
+    public void cry() {
+        System.out.println("Cat cry()小猫喵喵叫。。。。");
+    }
+}
---
+++ javabase/src/com/polymorphic/objectploy/Dog.java
@@ -0,0 +1,9 @@
+package com.polymorphic.objectploy;
+
+public class Dog extends Animal{
+
+    @Override
+    public void cry() {
+        System.out.println("Dog cry()小狗汪汪叫。。。。");
+    }
+}
