14a6620f0f3658eec3c3d6ad68e89c3ad3c334ae
chenchao <chenchao@qq.com>
2022-05-02 01:53:31 +0800

    零钱通-收益入账

--- javabase/src/com/smallchange/SmallChangeSys.java
+++ javabase/src/com/smallchange/SmallChangeSys.java
@@ -1,5 +1,7 @@
 package com.smallchange;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class SmallChangeSys {
@@ -8,6 +10,7 @@ public class SmallChangeSys {
      * @param args
      * 1.先完成显示菜单，并可以选择菜单，给出相应的提示
      * 2.完成零钱通明细
+     * 3.收益入账
      */
     public static void main(String[] args) {
 
@@ -22,6 +25,15 @@ public class SmallChangeSys {
          */
         //选择第（3）个方法
         String details = "----------零钱通明细----------";
+
+        /**
+         * 3.收益入账的思路
+         * （1）定义新的变量
+         */
+        double money = 0;
+        double balance = 0;
+        Date date = null;//date 是 java.util.Date 类型，表示日期
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");//用户日期格式化
 
         do {
             System.out.println("\n==========零钱通菜单==========");
@@ -39,7 +51,15 @@ public class SmallChangeSys {
                     System.out.println(details);
                     break;
                 case "2":
-                    System.out.println("2 收益入账");
+                    System.out.println("收益入账金额：");
+                    money = scanner.nextDouble();
+                    //money的值得范围 需要进行校验
+
+                    balance += money;
+                    //拼接收益入账信息到 details
+                    date = new Date();//获取当前日期
+                    details += "\n收益入账\t+" + money + "\t" + simpleDateFormat.format(date) + "\t" + "余额:" + balance;
+
                     break;
                 case "3":
                     System.out.println("3 消    费");
