231f159fa4136f7e5526be1a9b44428724dfa1d2
chenchao <chenchao@qq.com>
2022-05-10 01:51:33 +0800

    update codeblock guide

--- chapter10/src/com/lacheln/codeblock/CodeBlock01.java
+++ chapter10/src/com/lacheln/codeblock/CodeBlock01.java
@@ -2,6 +2,13 @@ package com.lacheln.codeblock;
 
 public class CodeBlock01 {
     public static void main(String[] args) {
+        Movie movie = new Movie("哈喽");
+        /*
+        电影屏幕打开。。。。
+        广告开始。。。。
+        电影正式开始。。。。
+        Movie(String name) 被调用
+         */
 
     }
 }
@@ -16,26 +23,26 @@ class Movie {
     1.下面的3个构造器都有相同的语句
     2.代码比较冗余
     3.可以将相同的语句，放入一个代码块中，即可
+    4.无论调用哪个构造器，都会先调用代码块的内容
+    5.代码块调用的优先级高于构造器
      */
-    public Movie(String name) {
+
+    {
         System.out.println("电影屏幕打开。。。。");
         System.out.println("广告开始。。。。");
         System.out.println("电影正式开始。。。。");
+    }
+    public Movie(String name) {
+        System.out.println("Movie(String name) 被调用");
         this.name = name;
     }
 
     public Movie(String name, double price) {
-        System.out.println("电影屏幕打开。。。。");
-        System.out.println("广告开始。。。。");
-        System.out.println("电影正式开始。。。。");
         this.name = name;
         this.price = price;
     }
 
     public Movie(String name, double price, String directory) {
-        System.out.println("电影屏幕打开。。。。");
-        System.out.println("广告开始。。。。");
-        System.out.println("电影正式开始。。。。");
         this.name = name;
         this.price = price;
         this.directory = directory;
