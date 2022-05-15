690b1ae7a0c95f79b8ced7480c6cba6b3409612f
chenchao <chenchao@qq.com>
2022-05-15 23:40:17 +0800

    接口多态性-多态参数

--- chapter10/src/com/lacheln/interface_/Computer.java
+++ chapter10/src/com/lacheln/interface_/Computer.java
@@ -2,6 +2,10 @@ package com.lacheln.interface_;
 
 public class Computer {
     //编写一个方法,计算机工作
+    /*
+    1.Usbinterface usbinterface 形参是接口类型 Usbinterface
+    2.看到 接收 实现了 Usbinterface 接口的类的对象实例
+     */
     public void work(Usbinterface usbinterface){
         //通过接口来调用方法
         usbinterface.statr();
--- chapter10/src/com/lacheln/interface_/Interface01.java
+++ chapter10/src/com/lacheln/interface_/Interface01.java
@@ -3,7 +3,9 @@ package com.lacheln.interface_;
 public class Interface01 {
     public static void main(String[] args) {
         //创建手机，相机
+        //Phone 实现了 Usbinterface
         Phone phone = new Phone();
+        //Camera 实现了 Usbinterface
         Camera camera = new Camera();
         //创建计算机
         Computer computer = new Computer();
