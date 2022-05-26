1d11afeded2e8000db5509eb5b8c3bd4c4ba134a
chenchao <chenchao@qq.com>
2022-05-26 14:29:20 +0800

    add homework

---
+++ chapter11/src/com/lacheln/homework/Homework08.java
@@ -0,0 +1,19 @@
+package com.lacheln.homework;
+
+/**
+ * Homework08
+ *
+ * @author lacheln
+ * @date 2022/5/26 11:24
+ * @since 1.0.0
+ */
+public class Homework08 {
+    public static void main(String[] args) {
+
+    }
+}
+
+//Color枚举类
+enum Color{
+    RED(),BULE(),BLACK(),YELLOW(),BLUE()
+}
