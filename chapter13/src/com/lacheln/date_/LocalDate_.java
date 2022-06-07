d08aea00306d98dd7a0368863c9762cb39179978
chenchao <742352449@qq.com>
2022-06-07 22:26:07 +0800

    update Localdate

--- chapter13/src/com/lacheln/date_/LocalDate_.java
+++ chapter13/src/com/lacheln/date_/LocalDate_.java
@@ -3,6 +3,7 @@ package com.lacheln.date_;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * LocatDate_
@@ -22,6 +23,12 @@ public class LocatDate_ {
         //返回当前时间
         LocalTime now2 = LocalTime.now();
         System.out.println(now2);
+
+        //使用
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH小时mm分钟ss秒");
+        String format = dtf.format(now);
+        System.out.println("格式化后的当前日期 = " + format);
+
         System.out.println("年：" + now.getYear());//年
         //数字月份
         System.out.println("月：" + now.getMonth());//月
@@ -32,5 +39,11 @@ public class LocatDate_ {
         System.out.println("分钟：" + now.getMinute());//分
         System.out.println("秒：" + now.getSecond());//秒
 
+        //提供了plus和minus方法 可以对当前时间进行 加 或者 减
+        LocalDateTime localDateTime = now.plusDays(890);
+        System.out.println("890天后 = " + dtf.format(localDateTime));
+
+        LocalDateTime minus = now.minusMinutes(3456);
+        System.out.println("3456分钟前 日期= " + dtf.format(minus));
     }
 }
---
+++ chapter13/src/com/lacheln/homework/Homework01.java
@@ -0,0 +1,16 @@
+package com.lacheln.homework;
+
+/**
+ * Homework01
+ *
+ * @author chenchao
+ * @date 2022/6/7 10:19 PM
+ * @since 1.0.0
+ */
+public class Homework01 {
+    public static void main(String[] args) {
+        //将字符串中，指定字符进行反转，比如：abcdef  反转未 aedcbf
+        String a = "abcdef";
+
+    }
+}
