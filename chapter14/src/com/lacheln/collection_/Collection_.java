f8983b13eefc3998f6f43ab584878e557f4921b0
chenchao <chenchao@qq.com>
2022-06-10 01:35:02 +0800

    14章 集合

---
+++ chapter14/src/com/lacheln/collection_/Collection_.java
@@ -0,0 +1,16 @@
+package com.lacheln.collection_;
+
+import java.util.Collection;
+
+/**
+ * Collection_
+ *
+ * @author lacheln
+ * @date 2022/6/10 01:29
+ * @since 1.0.0
+ */
+public class Collection_ {
+    public static void main(String[] args) {
+        Collection
+    }
+}
