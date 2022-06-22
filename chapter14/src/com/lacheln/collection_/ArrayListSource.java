dbb2790d09f3f5030edbb027c3fc0f05a2e72c7c
chenchao <742352449@qq.com>
2022-06-22 14:05:22 +0800

    add ArrayList LinkedList Vector底层源码分析、双向链接

--- chapter14/src/com/lacheln/collection_/ArrayListSource.java
+++ chapter14/src/com/lacheln/collection_/ArrayListSource.java
@@ -16,6 +16,25 @@ public class ArrayListSource {
         2) 当创建ArrayList对象时 ,如果使用的是无参构造器，则初始elementData容量为0, 第1次添加,则扩容elementData容量为10,如需要再次扩容，则扩容elementData容量为1.5倍
         3) 如果使用的是指定大小的构造器，则初始elementData容量为指定大小，如果需要扩容elementData容量为1.5倍.
          */
-        ArrayList arrayList = new ArrayList();
+
+        //使用无参构造器创建ArrayList对象
+        //2) 当创建ArrayList对象时 ,如果使用的是无参构造器，则初始elementData容量为0, 第1次添加,则扩容elementData容量为10,如需要再次扩容，则扩容elementData容量为1.5倍
+        ArrayList list = new ArrayList();
+
+        //3) 如果使用的是指定大小的构造器，则初始elementData容量为指定大小，如果需要扩容elementData容量为1.5倍.
+//        ArrayList list = new ArrayList(8);
+        //使用for循环给list集合添加1-10个数据
+        for (int i = 0; i <= 10; i++) {
+         list.add(i);
+        }
+        //使用for循环给list集合添加11-15个数据
+        for (int i = 11; i <= 15; i++) {
+            list.add(i);
+        }
+        list.add(100);
+        list.add(200);
+        list.add(null);
+
+
     }
 }
---
+++ chapter14/src/com/lacheln/list_/LinkedListCRUD.java
@@ -0,0 +1,14 @@
+package com.lacheln.list_;
+
+/**
+ * LinkedListCRUD
+ *
+ * @author chenchao
+ * @date 2022/6/22 2:02 PM
+ * @since 1.0.0
+ */
+public class LinkedListCRUD {
+    public static void main(String[] args) {
+
+    }
+}
