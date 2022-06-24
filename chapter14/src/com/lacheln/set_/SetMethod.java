a3f57c7b6b0c7ece1ceabf454fa89af563cdde07
chenchao <742352449@qq.com>
2022-06-24 20:21:01 +0800

    add SetMethod

---
+++ chapter14/src/com/lacheln/set_/SetMethod.java
@@ -0,0 +1,19 @@
+package com.lacheln.set_;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * SetMethod
+ *
+ * @author chenchao
+ * @date 2022/6/24 4:42 PM
+ * @since 1.0.0
+ */
+public class SetMethod {
+    public static void main(String[] args) {
+        //以Set接口实现类 HashSet为例
+        Set set = new HashSet();
+
+    }
+}
