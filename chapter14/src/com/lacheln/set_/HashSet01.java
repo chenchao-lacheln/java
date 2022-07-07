4ebad1e08e3b1fafdb9d66cae7ddfe85d8394aa0
chenchao <742352449@qq.com>
2022-07-07 19:11:08 +0800

    Update TestDemo

--- chapter14/src/com/lacheln/set_/HashSet01.java
+++ chapter14/src/com/lacheln/set_/HashSet01.java
@@ -1,5 +1,7 @@
 package com.lacheln.set_;
 
+import java.util.HashSet;
+
 /**
  * HashSet01
  *
@@ -9,5 +11,15 @@ package com.lacheln.set_;
  */
 public class HashSet01 {
     public static void main(String[] args) {
+        HashSet set = new HashSet();
+        // 在执行add方法后 会返回一个Boolean 值
+        // 添加成功返回TRUE 添加失败 返回FALSE
+        System.out.println(set.add("Jhon"));//T
+        System.out.println(set.add("Lucy"));//T
+        System.out.println(set.add("Jhon"));//F
+        System.out.println(set.add("Jack"));//T
+        System.out.println(set.add("lacheln"));//T
+
+        System.out.println("set = " + set);
     }
 }
--- chapter14/src/com/lacheln/set_/TestDemo.java
+++ chapter14/src/com/lacheln/set_/TestDemo.java
@@ -1,6 +1,10 @@
 package com.lacheln.set_;
 
+import java.math.BigDecimal;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * TestDemo
@@ -11,82 +15,88 @@ import java.text.SimpleDateFormat;
  */
 public class TestDemo {
     public static void main(String[] args) {
-        UpgradeCost upgradeCost = new UpgradeCost();
-        // 499 - (11.12 / 31) * ( 1659767956 - 1657098931)=487.92
-        double oneYearCla1 = upgradeCost.CostCalculation(499, 11.12, 2592000, 1657098931);
-        System.out.println("专业版新用户-原套餐为连续包月-1年 = " + oneYearCla1);
-        double oneYearCla2 = upgradeCost.CostCalculation(49.9, 11.12, 2592000, 1657098931);
-        System.out.println("专业版新用户-原套餐为连续包月-1个月 = " + oneYearCla2);
+        Tool tool = new Tool();
+        Money money = new Money();
+        //选择的套餐价格 收取的连续订阅价格 下次收费时间
+        System.out.println("专业版新用户-原套餐为连续包月-1年 = " + money.isMounthPay("499","11.12","1659767956"));
+        System.out.println("专业版新用户-原套餐为连续包月-1个月 = " + money.isMounthPay("49.9","11.12","1659767956"));
+        Date time = Tool.upgradeTime31();
+        //时间戳转化时间
+        tool.stampToTime(time);
 
+        //时间转换时间戳
+        try {
+            tool.dateToStamp("2022-08-07 18:25:14");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
     }
 }
-//1.升级日开始计算有效期
-//2.1年：价格为1年价格-【收费的连续订阅金额/31天*（下一次收费时间-升级时间）】；
 
 //升级费用计算测试
-class UpgradeCost {
-    //价格 原订单金额 天数 下次收费时间 升级时间
-    private double price;
-    private double OriginalOrderAmount;
-    private int day = 2592000; //31天
-    private long nextChargeTime;
-
-    public UpgradeCost() {
-    }
-
-    public UpgradeCost(double price, double originalOrderAmount, int day, long nextChargeTime) {
-        this.price = price;
-        OriginalOrderAmount = originalOrderAmount;
-        this.day = day;
-        this.nextChargeTime = nextChargeTime;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public double getOriginalOrderAmount() {
-        return OriginalOrderAmount;
-    }
-
-    public void setOriginalOrderAmount(double originalOrderAmount) {
-        OriginalOrderAmount = originalOrderAmount;
-    }
-
-    public int getDay() {
-        return day;
-    }
-
-    public void setDay(int day) {
-        this.day = day;
-    }
-
-    public long getNextChargeTime() {
-        return nextChargeTime;
-    }
-
-    public void setNextChargeTime(long nextChargeTime) {
-        this.nextChargeTime = nextChargeTime;
-    }
-
-    //update time
+class Tool {
+    //update time 获取当前时间
     public final static long upgradeTime(){
         return System.currentTimeMillis() / 1000 ;
     }
-
-    public void TimeChange(Long l){
+    //update time 获取当前时间 + 31天
+    public final static Date upgradeTime31(){
+        //获取系统当前时间
+        Calendar instance = Calendar.getInstance();
+        //+31天
+        instance.add(Calendar.DATE,31);
+        //得到最终时间
+        Date time = instance.getTime();
+        return time;
+    }
+    //时间戳转时间
+    public void stampToTime(Date date1){
         //time format conversion
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String strTime = simpleDateFormat.format(l);
-        System.out.println(strTime);
+        String strTime = simpleDateFormat.format(date1);
+        System.out.println("stampToTime = " + strTime);
     }
-    public double CostCalculation(double price,double originalOrderAmount,int day,long nextChargeTime ){
-        return price - (originalOrderAmount / day) * (nextChargeTime - upgradeTime());
+    //时间转时间戳
+    public void dateToStamp(String date2) throws ParseException {
+        String res;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = simpleDateFormat.parse(date2);
+        //转化为10位数时间戳
+        long time = date.getTime() / 1000;
+        res = String.valueOf(time);
+        System.out.println("dateToStamp = " + res);
+    }
+}
+
+//升级费用计算
+class Money {
+    //原套餐为连续包月
+    public BigDecimal isMounthPay(String price,String originalOrderAmount,String nextChargeTime){
+        BigDecimal a = new BigDecimal(price);
+        //收费的连续订阅金额
+        BigDecimal b = new BigDecimal(originalOrderAmount);
+        //31天
+        BigDecimal day1 = new BigDecimal("2592000"); //2592000
+        //下一次收费时间
+        BigDecimal c = new BigDecimal("1659767956");
+        //升级时间
+        BigDecimal upgradeTime = new BigDecimal(Tool.upgradeTime());
+
+        //金额计算
+        //收费的连续订阅金额/31天
+//        BigDecimal divide = originalOrderAmount.divide(day,10,BigDecimal.ROUND_DOWN);
+//        System.out.println("收费的连续订阅金额/31天 = " + divide);
+        //下一次收费时间-升级时间
+//        BigDecimal subtract = nextChargeTime.subtract(upgradeTime);
+//        System.out.println("下一次收费时间-升级时间 = " + subtract);
+        //结果
+//        BigDecimal multiply = divide.multiply(subtract);
+        return a.subtract(b.divide(day1, 10, BigDecimal.ROUND_DOWN).
+                        multiply(c.subtract(upgradeTime)));
+    }
+    //原套餐为非连续包月
+    public void isNotMounthPay(){
     }
 }
 
