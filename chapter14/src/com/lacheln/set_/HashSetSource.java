01f33b2e0ae362c9098a83f45e90348bd4dc0942
chenchao <chenchao@qq.com>
2022-07-09 22:15:50 +0800

    HashSet底层机制说明-第三次add分析

--- chapter14/src/com/lacheln/set_/HashSetSource.java
+++ chapter14/src/com/lacheln/set_/HashSetSource.java
@@ -56,14 +56,34 @@ public class HashSetSource {
                 if ((p = tab[i = (n - 1) & hash]) == null)
                     tab[i] = newNode(hash, key, value, null);
                 else {
-                    Node<K,V> e; K k;
+                    //开发技巧提示：在需要局部变量（辅助变量）的时候，再创建
+                    Node<K,V> e; K k; //定义辅助变量
+                    // 如果当前索引位置对用的链表的第一个元素和准备添加的key的hash值一样 -->> p.hash == hash
+                    //并且满足下面2个条件之一
+                    //(1).准备加入的key 和 p 指向的 Node结点的 key 是同一个对象  -->> (k = p.key) == key
+                    //(2).p 指向的 Node结点的 key 的equals() 和准备加入的key比较后相同 -->> key != null && key.equals(k)))
+                    //就不能加入
                     if (p.hash == hash &&
                         ((k = p.key) == key || (key != null && key.equals(k))))
                         e = p;
+                    //再判断 p 是不是一颗红黑树，
+                    //如果是一颗红黑树，就调用 putTreeVal 方法，来进行添加
                     else if (p instanceof TreeNode)
                         e = ((TreeNode<K,V>)p).putTreeVal(this, tab, hash, key, value);
+                    //如果table对应的索引位置，已经是一个链表，就使用for循环比较
+                    //(1)依次和该链表的每一个元素比较后，都不相同，则加入到该链表的最后   -->> p.next = newNode(hash, key, value, null);
+                    //      注意在把元素添加到链表后，立即判断，该链表是否已经达到8个结点  -->> if (binCount >= TREEIFY_THRESHOLD - 1) // -1 for 1st
+                    //      ，就调用 treeifyBin() 对当前这个链表进行树化（转成红黑树）
+                    //      注意，在转成红黑树时，要进行判断，判断条件如下
+                    //      if (tab == null || (n = tab.length) < MIN_TREEIFY_CAPACITY)  // MIN_TREEIFY_CAPACITY == 64
+                    //            resize();
+                    //      如果上面的条件成立，就先对table进行扩容
+                    //      只有上面的条件不成立时，才树化，即才转换成红黑树
+                    //(2)依次和该链表的每一个元素比较过程中，如果有相同的情况，就直接break
+                    //(3)
+                    //(4)
                     else {
-                        for (int binCount = 0; ; ++binCount) {
+                        for (int binCount = 0; ; ++binCount) { // 本质是一个死循环，退出的情况即如下，2个 ，break
                             if ((e = p.next) == null) {
                                 p.next = newNode(hash, key, value, null);
                                 if (binCount >= TREEIFY_THRESHOLD - 1) // -1 for 1st
