e5d4d88130ae997fdb314dc5feca46e31efa0729
chenchao <chenchao@qq.com>
2022-07-18 22:27:40 +0800

    Graphics绘制图形方法

--- chapter16/src/com/lacheln/draw/DrawCircle.java
+++ chapter16/src/com/lacheln/draw/DrawCircle.java
@@ -49,6 +49,36 @@ class MyPanel extends JPanel {
         super.paint(g); //调用父类方法，完成初始化
         System.out.println("paint方法被调用了");
         //画一个圆
-        g.drawOval(10,10,100,100);
+        //g.drawOval(10,10,100,100);
+        //演示绘制不同图形
+        //1.画直线drawLine(int x1,int y1,int x2,int y2)
+        //g.drawLine(10,10,100,100);
+
+        //2.画矩形边框drawRect(int X, int y, int width, int height)
+        //g.drawRect(10,10,100,100);
+
+        //3.画椭圆边框drawOval(int x, int y, int width, int height)
+
+        //4.填充矩形fillRect(int x, int y, int width, int height)
+        //设置画笔颜色
+        //g.setColor(Color.blue);
+        //g.fillRect(10,10,100,100);
+
+        //5.填充椭圆fillval(int x, int y, int width, int height)
+        //g.setColor(Color.red);
+        //g.fillRect(10,10,50,100);
+
+        //6.画图片drawlmage(Image img, int X, int y, .)
+        //6.1获取图片资源 /IMG_0797.JPG 表示在该项目的根目录去获取 IMG_0797.JPG 图片资源
+        //Image image = Toolkit.getDefaultToolkit().getImage(Panel.class.getResource("/IMG_0797.JPG"));
+        //g.drawImage(image,10,10,177,142,this);
+
+        //7.画字符串drawString(String str, int x, int y)
+        //设置画笔颜色和字体
+        g.setColor(Color.red);
+        g.setFont(new Font("隶属",Font.BOLD,50));
+        g.drawString("北京你好",100,100);// x,y对应左下角
+        //8.设置画笔的字体setFont(Font font)
+        //9.设置画笔的颜色setColor(Color c)
     }
 }
