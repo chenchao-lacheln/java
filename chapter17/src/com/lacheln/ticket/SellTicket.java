2153aaa84ecc9396d185965d079163829bc9781d
chenchao <chenchao@qq.com>
2022-07-22 00:49:11 +0800

    多线程执行

---
+++ chapter17/src/com/lacheln/ticket/SellTicket.java
@@ -0,0 +1,14 @@
+package com.lacheln.ticket;
+
+/**
+ * SellTicket
+ *
+ * @author lacheln
+ * @date 2022/7/22 00:48
+ * @since 1.0.0
+ */
+public class SellTicket {
+    public static void main(String[] args) {
+
+    }
+}
