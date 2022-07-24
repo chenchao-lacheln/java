b6301e41a43aea6f7cd3763d1c00a2d5c4c55f50
chenchao <chenchao@qq.com>
2022-07-25 02:19:34 +0800

    add InputStream.uml and creat director inputstream_

--- chapter19/src/com/lacheln/file/Director_.java
+++ chapter19/src/com/lacheln/file/Director_.java
@@ -63,4 +63,8 @@ public class Director_ {
             }
         }
     }
+//    InputStream
+//    OutputStream
+//    Reader
+//    Writer
 }
---
+++ chapter19/src/com/lacheln/inputstream_/FileInputStream_.java
@@ -0,0 +1,14 @@
+package com.lacheln.inputstream_;
+
+/**
+ * FileInputStream_
+ *
+ * @author lacheln
+ * @date 2022/7/25 02:18
+ * @since 1.0.0
+ */
+public class FileInputStream_ {
+    public static void main(String[] args) {
+
+    }
+}
