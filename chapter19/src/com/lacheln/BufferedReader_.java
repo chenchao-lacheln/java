97a688b46ea9ed2604cee377d7d2d947deebcc4c
chenchao <742352449@qq.com>
2022-10-18 18:44:40 +0800

    模拟修饰器设计模式--描述补充

--- chapter19/src/com/lacheln/BufferedReader_.java
+++ chapter19/src/com/lacheln/BufferedReader_.java
@@ -22,6 +22,8 @@ public class BufferedReader_ extends Reader_{
         reader_.readFile();
     }
     //让方法更加灵活，多次读取文件，或者加缓冲char[] ...
+    //可以增加缓存，可以提交输入输出的效率
+    //可以提供一些列便捷方法，使用更加灵活方便
     public void readFiles (int num){
         for (int i = 0;i < num;i++){
             reader_.readFile();
