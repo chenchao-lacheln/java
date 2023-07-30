c49b52c85a009fa7271827f421573b92cbe12414
lacheln <742352449@qq.com>
2023-07-30 18:26:53 +0800

    20章:io运用到坦克大战中

---
+++ chapter20/src/com/lacheln/tankgame05/EnemyTank.java
@@ -0,0 +1,85 @@
+package com.lacheln.tankgame05;
+
+import java.util.Vector;
+
+/**
+ * EnemyTank
+ * 敌人坦克
+ * @author lacheln
+ * @date 2022/7/19 08:39
+ * @since 1.0.0
+ */
+public class EnemyTank extends Tank implements Runnable{
+    //在敌人坦克类，使用Vector 保存多个Shot
+    Vector<Shot> shots = new Vector<>();
+    boolean isLive = true;
+
+    public EnemyTank(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void run() {
+        while (true){
+            //根据坦克的方向继续移动
+            switch (getDirect()){
+                case 0: //向上
+                    //让坦克保持一个方向，走30步
+                    for (int i = 0;i < 30;i++) {
+                        if (getY() > 0) {
+                            moveUp();
+                        }
+                        try {
+                            Thread.sleep(50);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+                case 1: //向右
+                    for (int i = 0;i < 30;i++) {
+                        if (getX() + 60 < 1000) {
+                            moveRight();
+                        }
+                        try {
+                            Thread.sleep(50);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+                case 2: //向下
+                    for (int i = 0; i < 30; i++) {
+                        if (getY() + 60 < 750) {
+                            moveDown();
+                        }
+                        try {
+                            Thread.sleep(50);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+                case 3: //向左
+                    for (int i = 0; i < 30; i++) {
+                        if (getX() > 0) {
+                            moveLeft();
+                        }
+                        try {
+                            Thread.sleep(50);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+            }
+
+            //然后 随机的改变坦克方向 0-3
+            setDirect((int) (Math.random() * 4));
+            //并发程序，一定要考虑清楚，该线程什么时候结束
+            if (!isLive){
+                break; //退出线程
+            }
+        }
+    }
+}
---
+++ chapter20/src/com/lacheln/tankgame05/LachelnTankGame5.java
@@ -0,0 +1,29 @@
+package com.lacheln.tankgame05;
+
+import javax.swing.*;
+
+/**
+ * LachelnTankGame01
+ *
+ * @author lacheln
+ * @date 2022/7/18 22:39
+ * @since 1.0.0
+ */
+public class LachelnTankGame5 extends JFrame {
+    //定义一个MyPanel
+    MyPanel mp = null;
+    public static void main(String[] args) {
+        LachelnTankGame5 lachelnTankGame05 = new LachelnTankGame5();
+
+    }
+    public LachelnTankGame5(){
+        mp = new MyPanel();
+        //将mp，放入到Thread 并 启动
+        new Thread(mp).start();
+        this.add(mp);//把面板（就是游戏的绘图区域）
+        this.setSize(1000,750);//设置外框大小
+        this.addKeyListener(mp); //让JFrame 监听 mp的键盘事件
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//关闭
+        this.setVisible(true);//显示
+    }
+}
