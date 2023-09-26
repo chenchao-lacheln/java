c4e25558509ad845250d01f3e4d1f5e1197e5840
chenchao <742352449@qq.com>
2023-09-27 02:01:08 +0800

    add:爆炸图片

--- chapter20/src/com/lacheln/tankgame05/LachelnTankGame5.java
+++ chapter20/src/com/lacheln/tankgame05/LachelnTankGame5.java
@@ -12,16 +12,18 @@ import javax.swing.*;
 public class LachelnTankGame5 extends JFrame {
     //定义一个MyPanel
     MyPanel mp = null;
+
     public static void main(String[] args) {
         LachelnTankGame5 lachelnTankGame05 = new LachelnTankGame5();
 
     }
-    public LachelnTankGame5(){
+
+    public LachelnTankGame5() {
         mp = new MyPanel();
         //将mp，放入到Thread 并 启动
         new Thread(mp).start();
         this.add(mp);//把面板（就是游戏的绘图区域）
-        this.setSize(1000,750);//设置外框大小
+        this.setSize(1000, 750);//设置外框大小
         this.addKeyListener(mp); //让JFrame 监听 mp的键盘事件
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//关闭
         this.setVisible(true);//显示
